ba5ab7387c8e0b738389940d13ff5523efdcf860
Lino Kalayil <linokalayil@Linos-MacBook-Air.local>
2024-11-03 20:56:51 -0600

    Initial commit for color picker app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-// Write your Color component here
+import React, { useState } from 'react';
+import './index.css';
+
+const Color = ({ color, setSelectedColor, isSelected }) => {
+  return (
+    <div
+      className={`${color} ${isSelected ? 'selected' : ''}`}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div>Currently selected:</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="yellow" setSelectedColor={setSelectedColor} isSelected={selectedColor === "yellow"} />
+        <Color color="green" setSelectedColor={setSelectedColor} isSelected={selectedColor === "green"} />
+        <Color color="red" setSelectedColor={setSelectedColor} isSelected={selectedColor === "red"} />
+      </div>
     </div>
   );
 };
 
 export default App;
+
+
+
